e8e3aa1e444582f1fa08f6918033eb40bf72db03
Haoyu Zhang <evin.zhang@mail.utoronto.ca>
2024-07-08 23:05:39 -0700

    updated contract and test to store challenge history

--- smart-contracts/contracts/Challenges.sol
+++ smart-contracts/contracts/Challenges.sol
@@ -17,77 +17,52 @@ contract Challenges {
         bytes32[] subChallenges; // Hashes of the sub-challenges
         string[] subSolutions;  // Solutions to the sub-challenges (empty initially)
         uint256 startBlock;  // block starting challenge
-        bool solved;
+        uint solved;
     }
 
     Challenge[] public challenges;
 
-    mapping(address => bytes32[]) public storedGroupHashes;
-
-    event GroupHashStored(address indexed recipient, string challengeText, bytes32[] groupHashArray);
-
-    event ChallengeCreated(uint indexed challengeId, address indexed recipient, string challengeText);
-    event SubChallengeSolved(uint indexed challengeId, uint indexed subChallengeIndex, bytes32 subSolution);
-    event ChallengeVerified(uint indexed challengeId, uint res);
+    event ChallengeCreated(address indexed recipient, string challengeText, bytes32[] groupHashArray, uint indexed challengeId);
+    event ChallengeSubmitted(uint indexed challengeId, uint solved);
     event log(string challengeId);
     event log2(bytes solved);
     event log3(bytes32 solved);
 
-    function storeGroupHash(address recipient, string memory challengeText, bytes32[] memory groupHashArray) public {
-        storedGroupHashes[recipient] = groupHashArray;
-        emit GroupHashStored(recipient, challengeText, groupHashArray);
-    }
-
-    function getGroupHash(address recipient) public view returns (bytes32[] memory) {
-        return storedGroupHashes[recipient];
-    }
-
     function createChallenge(address _recipient, string memory _challengeText, bytes32[] memory _subChallenges) public {
         require(_subChallenges.length == BIT_RANGE, "There must be exactly 35 sub-challenges.");
         string[] memory emptySolutions = new string[](BIT_RANGE);
+        uint challengeId = challenges.length;
         challenges.push(Challenge({
             recipient: _recipient,
             challengeText: _challengeText,
             subChallenges: _subChallenges,
             subSolutions: emptySolutions,
             startBlock: block.number,
-            solved: false
+            solved: 0
         }));
-        emit ChallengeCreated(challenges.length - 1, _recipient, _challengeText);
+        emit ChallengeCreated(_recipient, _challengeText, _subChallenges, challengeId);
     }
 
     function solveChallenge(uint _challengeIndex, string[] memory _subSolutions) public {
         require(_challengeIndex < challenges.length, "Challenge does not exist.");
         require(_subSolutions.length <= BIT_RANGE, "Too many solutions.");
         Challenge storage challenge = challenges[_challengeIndex];
-        require(!challenge.solved, "Challenge already solved.");
+        require(challenge.solved == 0, "Challenge already solved.");
         require(msg.sender == challenge.recipient, "Only the recipient can solve this challenge.");
         require(block.number <= challenge.startBlock+BLOCK_LIMIT, "Solutions can only be reported before block time limit");
 
         for (uint i = 0; i < _subSolutions.length; i++) {
             challenge.subSolutions[i] = _subSolutions[i];
-            emit SubChallengeSolved(_challengeIndex, i, "1");
         }
-    }
-
-    function verifyChallenge(uint _challengeIndex) public {
-        require(_challengeIndex < challenges.length, "Challenge does not exist.");
-        Challenge storage challenge = challenges[_challengeIndex];
-        uint i;
-        bool allSolved = true;
-        for (i = 0; i < BIT_RANGE; i++) {
-            // emit log(challenge.subSolutions[i]);
-            // emit log2(fromHex(challenge.subSolutions[i]));
-            // emit log3(sha256(fromHex(challenge.subSolutions[i])));
-            // emit log(challenge.subSolutions[i], keccak256(abi.encodePacked(challenge.subSolutions[i])));
-
-            if (sha256(fromHex(challenge.subSolutions[i])) != challenge.subChallenges[i]) {
-                allSolved = false;
+        uint j;
+        for (j = 0; j < _subSolutions.length; j++) {
+            if (sha256(fromHex(_subSolutions[j])) != challenge.subChallenges[j]) {
                 break;
             }
         }
-        challenge.solved = allSolved;
-        emit ChallengeVerified(_challengeIndex, i);
+        challenge.solved = j;
+        emit ChallengeSubmitted(_challengeIndex, j);
+
     }
 
    // Convert an hexadecimal character to their value
@@ -116,10 +91,10 @@ contract Challenges {
         return r;
     }
 
-    function getChallenge(uint _index) public view returns (address, string memory, bytes32[] memory, string[] memory, bool) {
+    function getChallenge(uint _index) public view returns (address, string memory, bytes32[] memory, string[] memory, uint256, uint) {
         require(_index < challenges.length, "Challenge does not exist.");
         Challenge storage ch = challenges[_index];
-        return (ch.recipient, ch.challengeText, ch.subChallenges, ch.subSolutions, ch.solved);
+        return (ch.recipient, ch.challengeText, ch.subChallenges, ch.subSolutions, ch.startBlock, ch.solved);
     }
 
     function getStartBit() public pure returns (uint256) {
--- smart-contracts/test/Challenges.js
+++ smart-contracts/test/Challenges.js
@@ -1,10 +1,10 @@
-const { expect } = require("chai");
+const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
 // const { keccak256, hexlify } = ethers.utils;
+const { waffle } = require("hardhat");
 
 describe("Challenges", function () {
   let Challenges;
-  let challenges;
   let owner;
   let addr1;
   let startBit;
@@ -29,23 +29,23 @@ describe("Challenges", function () {
 
   it("Should create a challenge with 35 sub-challenges for a specified recipient", async function () {
     const challengeText = "Solve the puzzle";
-    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.keccak256(ethers.getBytes("0x123456")));
+    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.utils.sha256(ethers.utils.arrayify("0x123456")));
     
     await expect(challenges.createChallenge(addr1.address, challengeText, subChallenges))
       .to.emit(challenges, "ChallengeCreated")
-      .withArgs(0, addr1.address, challengeText);
+      .withArgs(addr1.address, challengeText, subChallenges, 0);
 
     const challenge = await challenges.getChallenge(0);
     expect(challenge[0]).to.equal(addr1.address);
     expect(challenge[1]).to.equal(challengeText);
     expect(challenge[2].length).to.equal(challengeLength);
     expect(challenge[3].length).to.equal(challengeLength);
-    expect(challenge[4]).to.equal(false);
+    expect(challenge[5]).to.equal(0);
   });
 
   it("Should allow the specified recipient to submit multiple sub-solutions at once in hex format", async function () {
     const challengeText = "Solve the puzzle";
-    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.keccak256(ethers.getBytes("0x123456")));
+    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.utils.sha256(ethers.utils.arrayify("0x123456")));
     const subSolution = (i) => "123456"; // Remove 0x prefix
 
     await challenges.createChallenge(addr1.address, challengeText, subChallenges);
@@ -53,9 +53,8 @@ describe("Challenges", function () {
     const solutions = [subSolution(0), subSolution(1), subSolution(2)];
 
     await expect(challenges.connect(addr1).solveChallenge(0, solutions))
-      .to.emit(challenges, "SubChallengeSolved")
-      .and.to.emit(challenges, "SubChallengeSolved")
-      .and.to.emit(challenges, "SubChallengeSolved")
+      .to.emit(challenges, "ChallengeSubmitted")
+      .withArgs(0, 3)
 
     const challenge = await challenges.getChallenge(0);
 
@@ -63,12 +62,12 @@ describe("Challenges", function () {
     for (let i = 0; i < solutions.length; i++) {
       expect(challenge[3][i]).to.equal(`${"123456"}`);
     }
-    expect(challenge[4]).to.equal(false); // Challenge should not be fully solved yet
+    expect(challenge[5]).to.equal(3); // Challenge should not be fully solved yet
   });
 
   it("Should not allow the specified recipient to submit solutions beyond block limit", async function () {
     const challengeText = "Solve the puzzle";
-    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.keccak256(ethers.getBytes("0x123456")));
+    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.utils.keccak256(ethers.utils.arrayify("0x123456")));
     const subSolution = (i) => "123456"; // Remove 0x prefix
 
     await challenges.createChallenge(addr1.address, challengeText, subChallenges);
@@ -86,26 +85,52 @@ describe("Challenges", function () {
 
   it("Should verify the challenge and mark it as solved if all solutions are correct", async function () {
     const challengeText = "Solve the puzzle";
-    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.sha256(ethers.getBytes("0x123456")));
+    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.utils.sha256(ethers.utils.arrayify("0x123456")));
     const subSolution = (i) => "123456"; // Remove 0x prefix
 
     await challenges.createChallenge(addr1.address, challengeText, subChallenges);
 
     const solutions = Array.from({ length: challengeLength }, (_, i) => subSolution(i));
 
-    await challenges.connect(addr1).solveChallenge(0, solutions);
-
-    await expect(challenges.verifyChallenge(0))
-      .to.emit(challenges, "ChallengeVerified")
-      .withArgs(0, challengeLength);
+    await expect(challenges.connect(addr1).solveChallenge(0, solutions))
+    .to.emit(challenges, "ChallengeSubmitted")
+    .withArgs(0, challengeLength);
 
     const challenge = await challenges.getChallenge(0);
-    expect(challenge[4]).to.equal(true); // Challenge should be marked as solved
+    expect(challenge[5]).to.equal(challengeLength); // Challenge should be marked as solved
   });
+
+  
+  it("Should create and solve multiple challenges", async function () {
+    const challengeText = "Solve the puzzle";
+    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.utils.sha256(ethers.utils.arrayify("0x123456")));
+    const subSolution = (i) => "123456"; // Remove 0x prefix
+
+    await challenges.createChallenge(addr1.address, challengeText, subChallenges);
+
+    const solutions = Array.from({ length: challengeLength }, (_, i) => subSolution(i));
+
+    await expect(challenges.connect(addr1).solveChallenge(0, solutions))
+    .to.emit(challenges, "ChallengeSubmitted")
+    .withArgs(0, challengeLength);
+
+    const challenge = await challenges.getChallenge(0);
+    expect(challenge[5]).to.equal(challengeLength); // Challenge should be marked as solved
+
+    // new challenge
+    await challenges.createChallenge(addr1.address, challengeText, subChallenges);
+    await expect(challenges.connect(addr1).solveChallenge(1, solutions))
+    .to.emit(challenges, "ChallengeSubmitted")
+    .withArgs(1, challengeLength);
+
+    const challenge1 = await challenges.getChallenge(1);
+    expect(challenge1[5]).to.equal(challengeLength); // Challenge should be marked as solved
+  });
+
 
   it("Should not verify the challenge if any solution is incorrect", async function () {
     const challengeText = "Solve the puzzle";
-    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.sha256(ethers.getBytes(`0x666666`)));
+    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.utils.sha256(ethers.utils.arrayify(`0x666666`)));
     const subSolution = (i) => "123456";
 
     await challenges.createChallenge(addr1.address, challengeText, subChallenges);
@@ -113,19 +138,17 @@ describe("Challenges", function () {
     const solutions = Array.from({ length: challengeLength }, (_, i) => subSolution(i));
     solutions[0] = "123456"; // Introduce an incorrect solution
 
-    await challenges.connect(addr1).solveChallenge(0, solutions);
-
-    await expect(challenges.verifyChallenge(0))
-      .to.emit(challenges, "ChallengeVerified")
+    await expect(challenges.connect(addr1).solveChallenge(0, solutions))
+      .to.emit(challenges, "ChallengeSubmitted")
       .withArgs(0, 0);
 
     const challenge = await challenges.getChallenge(0);
-    expect(challenge[4]).to.equal(false); // Challenge should not be marked as solved
+    expect(challenge[5]).to.equal(0); // Challenge should not be marked as solved
   });
 
   it("Should not allow a non-recipient to submit sub-solutions", async function () {
     const challengeText = "Solve the puzzle";
-    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.keccak256(ethers.toUtf8Bytes(`Sub-challenge ${i}`)));
+    const subChallenges = Array(challengeLength).fill().map((_, i) => ethers.utils.keccak256(ethers.utils.toUtf8Bytes(`Sub-challenge ${i}`)));
     const subSolution = (i) => `Sub-challenge ${i}`;
 
     await challenges.createChallenge(addr1.address, challengeText, subChallenges);
